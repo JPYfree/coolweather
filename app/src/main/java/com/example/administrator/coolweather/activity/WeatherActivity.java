d6889e9feaf6c1fdc212cca5606b0ff6e752d2e1
JPYfree <jpyfreeprogram@163.com>
2015-07-21 11:00:15 +0800

    更新后台自动更新天气的功能

--- app/src/main/java/com/example/administrator/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/activity/WeatherActivity.java
@@ -13,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.administrator.coolweather.R;
+import com.example.administrator.coolweather.service.AutoUpdateService;
 import com.example.administrator.coolweather.util.HttpCallbackListener;
 import com.example.administrator.coolweather.util.HttpUtil;
 import com.example.administrator.coolweather.util.Utility;
@@ -165,5 +166,7 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         currentDateText.setText(prefs.getString("current_date", ""));
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
